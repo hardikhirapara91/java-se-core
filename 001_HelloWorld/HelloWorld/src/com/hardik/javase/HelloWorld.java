54cef12b17abafd957c8e383a766dc2088f05305
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-06-26 13:52:07 +0530

    Hello World Example

---
+++ 001_HelloWorld/HelloWorld/src/com/hardik/javase/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hardik.javase;
+
+/**
+ * Application Main HelloWorld Class
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
