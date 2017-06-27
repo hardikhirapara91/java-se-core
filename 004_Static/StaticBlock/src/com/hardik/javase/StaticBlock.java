17aa338f77c3126dae72e2c8a78dfd5a24a081dd
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-06-27 18:16:00 +0530

    Java Static Block Example.

---
+++ 004_Static/StaticBlock/src/com/hardik/javase/StaticBlock.java
@@ -0,0 +1,25 @@
+package com.hardik.javase;
+
+/**
+ * Static Block
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class StaticBlock {
+
+	public static String name;
+
+	// Static block is used to Initialize static data members.
+	static {
+		name = "Hardik Hirapara";
+		System.out.println("Static block is initialized.");
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Name: " + name);
+
+	}
+
+}
