ce6829e0bc0280ddd9fa1377078cf84e26dee1d0
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-06-27 18:36:59 +0530

    Constructor Chaining Example.

---
+++ 005_Constructors/ConstructorChaining/src/com/hardik/hirapara/ConstructorChaining.java
@@ -0,0 +1,17 @@
+package com.hardik.hirapara;
+
+/**
+ * Constructor Chaining
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class ConstructorChaining {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student(25, "Hardik", "Hirapara", "Mumbai");
+		System.out.println(s1);
+	}
+
+}
