85b70ca98e20955a1311b594e3f335c001e1e9d9
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-03 17:02:27 +0530

    Character unicode example

---
+++ 012_Data Types/CharUnicode/src/com/hardik/javase/CharacterLiterals.java
@@ -0,0 +1,18 @@
+package com.hardik.javase;
+
+/**
+ * Character Literals
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class CharacterLiterals {
+
+	public static void main(String[] args) {
+
+		char c = '\u0061'; // ISO-Latin-1 'a'
+		System.out.println("\\u0061: " + c);
+
+	}
+
+}
