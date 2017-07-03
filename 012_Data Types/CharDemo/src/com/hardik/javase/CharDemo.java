b37f42849f7787aeb4e2d63762a16b458b0a5875
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-03 17:05:11 +0530

    Java character data type example

---
+++ 012_Data Types/CharDemo/src/com/hardik/javase/CharDemo.java
@@ -0,0 +1,25 @@
+package com.hardik.javase;
+
+/**
+ * Character Data-Type Example
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class CharDemo {
+
+	public static void main(String[] args) {
+
+		char ch1 = 65;
+		char ch2 = 'B';
+
+		System.out.println("ch1: " + ch1);
+		System.out.println("ch2: " + ch2);
+
+		ch1++;
+		ch2--;
+
+		System.out.println("Now ch1: " + ch1);
+		System.out.println("Now ch2: " + ch2);
+	}
+}
