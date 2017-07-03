a08fd234f2951ad6cc58591474783013e8a3662c
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-03 16:21:52 +0530

    Float literals

---
+++ 012_Data Types/FloatLiterals/src/com/hardik/javase/FloatLiterals.java
@@ -0,0 +1,16 @@
+package com.hardik.javase;
+
+/**
+ * Double Literals
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class FloatLiterals {
+
+	public static void main(String[] args) {
+
+		float f = 12345.678f; // Must append F or f
+		System.out.println("Float Literals: " + f);
+	}
+}
