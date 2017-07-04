adaa150279f3f9c47bca1e5b88e1b740caae691e
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-04 11:12:05 +0530

    Java this keyword example

---
+++ 015_This Keyword/Demo1/src/com/hardik/javase/JavaThisKeyword.java
@@ -0,0 +1,16 @@
+package com.hardik.javase;
+
+/**
+ * Java This Keyword
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class JavaThisKeyword {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student(25, "Hardik");
+		
+		System.out.println(s1);
+	}
+}
