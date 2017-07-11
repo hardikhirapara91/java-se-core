90623dedb1df654e15688180bbe0586100cb3c73
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-11 17:05:44 +0530

    Java callback example

---
+++ 032_Callback/Demo1/src/com/hardik/javase/CallBack.java
@@ -0,0 +1,16 @@
+package com.hardik.javase;
+
+/**
+ * CallBack Class
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class CallBack {
+
+	public static void main(String[] args) {
+
+		Boss boss = new Boss();
+		boss.directMethod();
+	}
+}
