0942ee295dce4291a2ac78a6795bfd4bbc3392d9
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-08-17 14:17:52 +0530

    Java while loop example

---
+++ 010_Looping/While/Demo1/src/main/java/com/hardik/javase/App.java
@@ -0,0 +1,17 @@
+package com.hardik.javase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		while (i <= 10) {
+			System.out.println("I: " + i);
+			i++;
+		}
+	}
+}
