ade248d19d838bc0cfbc7d640236037610338087
Hardik Hirapara <hardikh@silentinfotech.com>
2017-09-26 10:43:50 +0530

    JavaSE infinitive for loop example.

---
+++ 010_Looping/For/InfinitiveForLoop/src/main/java/com/hardik/javase/App.java
@@ -0,0 +1,15 @@
+package com.hardik.javase;
+
+/**
+ * Java Infinitive For Loop Example
+ * 
+ * @author HARDIK HIRAPARA
+ */
+public class App {
+	public static void main(String[] args) {
+
+		for (;;) {
+			System.out.println("This is infinitive for-loop in java.");
+		}
+	}
+}
