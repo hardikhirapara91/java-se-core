e09e052628938afe569565e5977af1e45655245e
Hardik Hirapara <hardikh@silentinfotech.com>
2017-09-26 13:52:27 +0530

    Java multi line comment example.

---
+++ 047_Java Comments/Multi-Line/MultiLineComment/src/main/java/com/hardik/javase/App.java
@@ -0,0 +1,22 @@
+package com.hardik.javase;
+
+/**
+ * Java Multi-Line Comment Example
+ * 
+ * The java comments are statements that are not executed by the compiler and
+ * interpreter. The comments can be used to provide information or explanation
+ * about the variable, method, class or any statement.
+ * 
+ * @author HARDIK HIRAPARA
+ */
+public class App {
+	public static void main(String[] args) {
+
+		/**
+		 * This is java's multi-line comment
+		 * 
+		 * We can write multiple content on this comment.
+		 */
+		System.out.println("Multi Line Comment.");
+	}
+}
