4901ca8f6657c7cec7f1072d0c4d0f9bdbed1fef
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-09-27 18:14:39 +0530

    Add copy-constructor on Student class.

--- 006_Constructor/CopyConstructor/src/com/hardik/hirapara/Student.java
+++ 006_Constructor/CopyConstructor/src/com/hardik/hirapara/Student.java
@@ -12,12 +12,15 @@ public class Student {
 	
 	public Student(){}
 	
+	// A normal parametrized constructor
 	public Student(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
+	// Copy constructor
 	public Student(Student s1) {
+		System.out.println("Copy constructor called");
 		this.id = s1.id;
 		this.name = s1.name;
 	}
