151a10fcad92de19e9b832ed4d6ea631c1af1891
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-10-10 12:33:29 +0530

    JavaSE recursion example for infinite times calling function.

---
+++ 042_Recursion/InfiniteTimes/src/main/java/com/hardik/javase/App.java
@@ -0,0 +1,30 @@
+package com.hardik.javase;
+
+/**
+ * Java Array Copy Example
+ * 
+ * @author HARDIK HIRAPARA
+ */
+public class App {
+
+	private static int counter = 0;
+	
+	/**
+	 * Recursion print method
+	 */
+	static void print() {
+		System.out.println("Print method calling:" + counter);
+		counter++;
+		
+		print();	// Method call itself
+	}
+	
+	/**
+	 * App Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		print();
+	}
+
+}
