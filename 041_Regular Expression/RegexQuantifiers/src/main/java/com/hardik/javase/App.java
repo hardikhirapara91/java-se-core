d6ef77f06b5a743bea05658fd8a2081eee464382
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-10-13 14:32:45 +0530

    Java regular expression with quantifier example.

---
+++ 041_Regular Expression/RegexQuantifiers/src/main/java/com/hardik/javase/App.java
@@ -0,0 +1,39 @@
+package com.hardik.javase;
+
+import java.util.regex.Pattern;
+
+/**
+ * Java Regular Expression Qquantifiers Example
+ * 
+ * @author HARDIK HIRAPARA
+ */
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("? quantifier ....");
+		// true (a or m or n comes one time)
+		System.out.println(Pattern.matches("[amn]?", "a"));
+		// false (a comes more than one time)
+		System.out.println(Pattern.matches("[amn]?", "aaa"));
+		// false (a m and n comes more than one time)
+		System.out.println(Pattern.matches("[amn]?", "aammmnn"));
+		// false (a comes more than one time)
+		System.out.println(Pattern.matches("[amn]?", "aazzta"));
+		// false (a or m or n must come one time)
+		System.out.println(Pattern.matches("[amn]?", "am"));
+
+		System.out.println("+ quantifier ....");
+		// true (a or m or n once or more times)
+		System.out.println(Pattern.matches("[amn]+", "a"));
+		// true (a comes more than one time)
+		System.out.println(Pattern.matches("[amn]+", "aaa"));
+		// true (a or m or n comes more than once)
+		System.out.println(Pattern.matches("[amn]+", "aammmnn"));
+		// false (z and t are not matching pattern)
+		System.out.println(Pattern.matches("[amn]+", "aazzta"));
+
+		System.out.println("* quantifier ....");
+		// true (a or m or n may come zero or more times)
+		System.out.println(Pattern.matches("[amn]*", "ammmna"));
+	}
+}
