c78137924f1a8d7d98e4dd7798a3016a7d70c479
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-11-01 18:53:17 +0530

    Modified some code in all projects.

--- 024_InstanceOf/InstanceOfDemo/src/main/java/com/hardik/javase/App.java
+++ 024_InstanceOf/InstanceOfDemo/src/main/java/com/hardik/javase/App.java
@@ -2,26 +2,35 @@ package com.hardik.javase;
 
 /**
  * Java InstanceOf Operator Example
- *
+ * 
+ * @author HARDIK HIRAPARA
  */
 public class App {
+
+	/**
+	 * Application Main Method
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+
 		Test t = new Test();
 		System.out.println("t => Test: " + (t instanceof Test));
-		
+
 		Dog d = new Dog();
 		System.out.println("d => Dog: " + (d instanceof Dog));
 		System.out.println("d => Animal:" + (d instanceof Animal));
-		
+
 		// instanceof in java with a variable that have null value
 		Test t1 = null;
-		System.out.println("t1 => null: "+(t1 instanceof Test));
-		
+		System.out.println("t1 => null: " + (t1 instanceof Test));
+
 		// Downcasting with java instanceof operator
-		// When Subclass type refers to the object of Parent class, it is known as downcasting.
-		// Dog d1 = new Animal();	// Compilation error
-		// Dog d1 = (Dog) new Animal();	// ClassCastException is thrown at runtime
+		// When Subclass type refers to the object of Parent class, it is known
+		// as downcasting.
+		// Dog d1 = new Animal(); // Compilation error
+		// Dog d1 = (Dog) new Animal(); // ClassCastException is thrown at
+		// runtime
 		Animal a1 = new Dog();
 		Dog.downcasting(a1);
 	}
