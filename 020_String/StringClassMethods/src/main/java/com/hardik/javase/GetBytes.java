af454c7bd03112d5df75804e1681e701677b78e8
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-11-21 14:26:22 +0530

    JavaSE String.getBytes() example. Added new topics on Notes.txt file.

--- 020_String/StringClassMethods/src/main/java/com/hardik/javase/GetBytes.java
+++ 020_String/StringClassMethods/src/main/java/com/hardik/javase/GetBytes.java
@@ -18,5 +18,32 @@ public class GetBytes {
 		for (int i = 0; i < byteArr.length; i++) {
 			System.out.println(byteArr[i]);
 		}
+
+		// Default CHARSET
+		String str2 = "I like JAVA !";
+        
+        byte [] test2 = str2.getBytes(Charset.defaultCharset());
+
+        for (int j=0; j<test2.length; j++) {
+            System.out.print("" + test2[j] + ",");
+        }
+
+        // String with numbers and some special characters
+        String str3 = "!$0123@";
+
+        // byte array with charset
+        byte bval[] = new byte[0];
+
+        try {
+            bval = str3.getBytes("UTF8");
+
+            // Prints the byte array
+            for (int i = 0; i < bval.length; i++) {
+                System.out.println(bval[i]);
+            }
+
+        } catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+        }
 	}
 }
